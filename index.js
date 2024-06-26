0291017695c7837a6476ec5d3eef4e9c6faa0be5
Niraj Parmar <nirajprmr1608@gmail.com>
2024-06-26 15:36:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-
 const express = require('express');
 const path = require('path');
-
-
 const port = 3000;
 const app = express();
-
-
-
-
-
 // Serve static files from the 'view' directory
 
 app.use(express.static(path.join(__dirname, 'views')));
 // Serve static files from the 'uploads' directory for JS files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-
-
-
-
-
 // Define routes for your application
 // Route to serve index.html
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index2.html'));
+});
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index2.html'));
 });
 
